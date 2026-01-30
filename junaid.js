bb1f2d5d8f52b26a78df8d35f99fa6a7c7d74670
abdullah <you@example.com>
2026-01-30 11:20:18 +0500

    fear

--- junaid.js
+++ junaid.js
@@ -11,18 +11,18 @@ console.log(x)
 junaid=45;
 console.log(junaid)
 
-a=455666
-b=66598585
-c=8999
-e=868
-f=588688
-d=a+b-c+e+f
-console.log(d);
+// a=455666
+// b=66598585
+// c=8999
+// e=868
+// f=588688
+// d=a+b-c+e+f
+// console.log(d);
 
-console.log("junaid")
+// console.log("junaid")
 
-console.log("zain nasserr")
-console.log("junaid khan")
-console.log("abdullah saeed")
+// console.log("zain nasserr")
+// console.log("junaid khan")
+// console.log("abdullah saeed")
 
 
